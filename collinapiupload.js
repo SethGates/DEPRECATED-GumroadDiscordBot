8b7fb01e16b3bc6031803dbb4e89e067aef9179e
Jason-Newell <jn.develop@icloud.com>
2022-11-01 13:00:36 -0700

    initial create of bot

--- collinapiupload.js
+++ collinapiupload.js
@@ -1,28 +1,82 @@
-const { Client, GatewayIntentBits } = require('discord.js')
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        // ...
-    ]
-})
+const https = require("https");
+const fs = require("fs");
 
+function ckGr() {
+  const grToken = process.env.grToken;
+  const url = `https://api.gumroad.com/v2/products?access_token=${grToken}`;
+  const lastProductIds = process.env.lastProductIds;
+  let currentProductIds = [];
+  let productsUpdated = false;
 
+  const request = https.request(url, (response) => {
+    let data = "";
+    response.on("data", (chunk) => {
+      data = data + chunk.toString();
+    });
 
-client.once('ready', () => {
-    console.log('Collins Content bot is Online!');
-});
+    response.on("end", () => {
+      const body = JSON.parse(data);
+      //console.log(data.products);
+      if (body.success) {
 
+        for (product of body.products) {
+          currentProductIds.push(product.id);
 
+          if (lastProductIds.indexOf(product.id) < 0) {
+            console.log('New product found');
+            productsUpdated = true;
+          }
+        }
 
+        if (productsUpdated) {
+          // if there are new products, update the environment varable with new list of products ids
+          console.log("New products is true");
+          updateProductList(JSON.stringify(currentProductIds));
+          sendBotMsg();
+          console.log("New products were found."); // log to console so we can see results in monitor console
+        } else { 
+            console.log("No updates found");
+        }
+      }
+    });
+  });
 
+  request.on("error", (error) => {
+    console.log("An error", error);
+  });
 
+  request.end();
+ 
+}
 
+function updateProductList(data) {
+  process.env.currentProductIds = date;
+}
 
-/* 
-Put this ...
-AT THE END !!!
-*/
+function sendBotMsg() {
+  console.log("Notifying bot");
+  const dcWhId = process.env.dcWhId;
+  const dcWhToken = process.env.dcWhToken;
+  let url = `https://discord.com/api/webhooks/${dcWhId}/${dcWhToken}`;
 
+  const request = https.request(url, (response) => {
+    let data = "";
+    response.on("data", (chunk) => {
+      data = data + chunk.toString();
+    });
 
-client.login('your-token-here')
+    response.on("end", () => {
+      const body = JSON.parse(data);
+      console.log(body);
+    });
+  });
 
+  request.on("error", (error) => {
+    console.log("An error", error);
+  });
+
+  request.end();
+}
+
+const interval = 216000;  // every 60 mins
+setInterval(ckGr, interval);
